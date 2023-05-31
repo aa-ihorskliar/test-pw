d4ccb82ee1665457c067f3ac033d97708acfd457
aa-ihorskliar <121878975+aa-ihorskliar@users.noreply.github.com>
2023-05-31 14:50:05 -0400

    test

--- testing/playwright/pages/HomePage.ts
+++
@@ -1,9 +0,0 @@
-import { BasicPage } from 'playwright/pages/core/BasicPage';
-import { URL_CONSTANTS } from 'playwright/constants';
-
-/**
- *	Home page class.
- */
-export class HomePage extends BasicPage {
-	readonly url: string = URL_CONSTANTS.home;
-}
